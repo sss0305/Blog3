f4ad434b78a2123b3ff505cc1b7ce4bb97e2267d
sss0305 <exploremexicoguide@gmail.com>
2022-06-21 19:17:41 -0500

    Init db, make @db

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+#инициализация глобальной переменной базы данных
+def init_db
+ @db = SQLite3::Database.new 'blog.db'
+ @db.results_as_hash = true 
+end
+
+before do
+ init_db
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
